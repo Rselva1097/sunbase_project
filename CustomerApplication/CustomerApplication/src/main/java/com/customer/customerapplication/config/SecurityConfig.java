ec424787d79e87d375cc76b3ad271f48d232d6e7
Rselva1097 <99159175+Rselva1097@users.noreply.github.com>
2024-04-05 07:56:37 -0400

    Update SecurityConfig.java

--- CustomerApplication/CustomerApplication/src/main/java/com/customer/customerapplication/config/SecurityConfig.java
+++ CustomerApplication/CustomerApplication/src/main/java/com/customer/customerapplication/config/SecurityConfig.java
@@ -54,9 +54,7 @@ public class SecurityConfig {
 
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                        //authorize.anyRequest().authenticated()
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-                                //.requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
 
